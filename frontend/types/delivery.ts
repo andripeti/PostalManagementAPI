aa08e5b106d674f9da32edbde8859d1a4f1faa51
Mateo Kurti <contact@mateokurti.com>
2025-02-17 17:03:45 +0100

    final touch frontend

---
+++ frontend/types/delivery.ts
@@ -0,0 +1,12 @@
+export interface DeliveryFormDto {
+  recipientFirstName?: string
+  recipientLastName?: string
+  recipientEmail?: string
+  recipientAddressStreet?: string
+  recipientAddressCity?: string
+  recipientAddressZipCode?: string
+  senderId?: number
+  holderId?: number
+  recipientId?: number
+}
+
