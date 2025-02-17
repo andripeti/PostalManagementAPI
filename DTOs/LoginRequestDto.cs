eaf08dbc0f8e26584470c7a4183249e29e12c7cb
Dea Peka <dea.pekaa@gmail.com>
2025-02-17 16:17:40 +0100

    create login request dto

---
+++ DTOs/LoginRequestDto.cs
@@ -0,0 +1,10 @@
+﻿namespace PostalManagementAPI.ViewModels
+{
+    public class LoginRequestDto
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public bool RememberMe { get; set; }
+    }
+
+}
