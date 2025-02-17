0549a42538b56fd1f05b66caf58b7548e2453d00
Mateo Kurti <contact@mateokurti.com>
2025-02-17 16:37:22 +0100

    add remaining database models

---
+++ Models/UserAddress.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PostalManagementAPI.Models
+{
+    [Table("tbl_user_addresses")]
+    public class UserAddress
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        [ForeignKey("User")]
+        public int UserId { get; set; }
+        [ForeignKey("Address")]
+        public int AddressId { get; set; }
+    }
+}
