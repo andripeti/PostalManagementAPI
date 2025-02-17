03ae3f07a4d287f4d78506ea0eeb1c65e964ba24
Andri Peti <apeti20@epoka.edu.al>
2025-02-17 16:36:26 +0100

    new: Get Delivery Form Values from Body

---
+++ DTOs/DeliveryFormDto.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PostalManagementAPI.DTOs
+{
+    public class DeliveryFormDto
+    {
+        // Sender Information (Provided by user)
+        [Required] public string SenderName { get; set; }
+        [Required] public string SenderPhone { get; set; }
+        [Required] public string SenderEmail { get; set; }
+        public string SenderStreet { get; set; }
+        public string SenderCity { get; set; }
+        public string SenderState { get; set; }
+        public string SenderCountry { get; set; }
+        public string SenderZipCode { get; set; }
+
+        // Recipient Information
+        [Required] public string RecipientName { get; set; }
+        [Required] public string RecipientPhone { get; set; }
+        [Required] public string RecipientEmail { get; set; }
+        [Required] public string RecipientStreet { get; set; }
+        [Required] public string RecipientCity { get; set; }
+        public string RecipientState { get; set; }
+        public string RecipientCountry { get; set; }
+        [Required] public string RecipientZipCode { get; set; }
+
+        // Package Information
+        [Required] public string PackageType { get; set; }
+        [Required] public float PackageWeight { get; set; }
+        public float PackageLength { get; set; }
+        public float PackageWidth { get; set; }
+        public float PackageHeight { get; set; }
+
+        // Notes (Optional)
+        public string Notes { get; set; }
+    }
+}
