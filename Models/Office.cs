1bad6037770edd91b167684e7be22bd5643a2c4d
Arta Keçi <arta.keci@fshnstudent.info>
2025-02-17 16:23:40 +0100

    office model for db

---
+++ Models/Office.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PostalManagementAPI.Models
+{
+    [Table("tbl_offices")]
+    public class Office
+    {
+        [Key]
+        public int Id { get; set; } 
+        public string Name { get; set; }
+        [ForeignKey("Address")]
+        public int AddressId { get; set; }
+        public string Phone { get; set; }
+        public string Type { get; set; }
+        public virtual Address Address { get; set; }
+    }
+}
