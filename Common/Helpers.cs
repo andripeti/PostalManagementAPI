3fcc972c5d2a4e5839d924d159e60435e0d0cf02
Arta Keçi <arta.keci@fshnstudent.info>
2025-02-17 16:19:02 +0100

    Create helper functions

---
+++ Common/Helpers.cs
@@ -0,0 +1,35 @@
+﻿using PostalManagementAPI.Services;
+
+public class Helpers
+{
+    private readonly DeliveryService _deliveryService;
+
+    private static Random _random = new Random();
+
+    public Helpers(DeliveryService deliveryService)
+    {
+        _deliveryService = deliveryService;
+    }
+
+    public static string GenerateTrackingNumber()
+    {
+        long number = (long)(_random.Next(100000, 999999) * 1_000_000) + _random.Next(100000, 999999);
+
+        return number.ToString();
+    }
+
+    public async Task<string> GenerateUniqueTrackingNumberAsync()
+    {
+        string trackingNumber;
+        bool exists;
+
+        do
+        {
+            trackingNumber = GenerateTrackingNumber();
+            exists = await _deliveryService.GetByTrackingNumberAsync(trackingNumber) != null;
+        }
+        while (exists);
+
+        return trackingNumber;
+    }
+}
